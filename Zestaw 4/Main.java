b82c51139d04d753c77bbca8d2960eaaeed5313a
DominikaMaciag <Dominika.s45@wp.pl>
2022-11-12 22:56:36 +0100

    implemented Polybius methods

--- Zestaw 4/Main.java
+++ Zestaw 4/Main.java
@@ -5,10 +5,18 @@ public class Main {
         // Cryptographer obj1 = new Cryptographer();
         // obj1.cryptfile("tocrypt.txt", "decrypt.txt", null);
 
+        System.out.println("\n"+"Test ROT11:");
         String test = "I 12 lat.";
         String test2 = "T CD wl4.";
         ROT11 r1 = new ROT11();
         System.out.println(r1.crypt(test));
         System.out.println(r1.decrypt(test2));
+
+        System.out.println("\n"+"Test Polibiusz:");
+        String test3 = "Ala ma kota";
+        String test4 = "113111 3211 25344411";
+        Polibiusz pol1 = new Polibiusz();
+        System.out.println(pol1.crypt(test3));
+        System.out.println(pol1.decrypt(test4)); 
     }
 }
--- Zestaw 4/Polibiusz.java
+++ Zestaw 4/Polibiusz.java
@@ -1,15 +1,50 @@
 public class Polibiusz implements Algorithm {
+        public char polybiusSquare[][] = { 
+        {'A', 'B', 'C', 'D', 'E'},
+        {'F', 'G', 'H', 'I', 'K'},
+        {'L', 'M', 'N', 'O', 'P'},
+        {'Q', 'R', 'S', 'T', 'U'},
+        {'V', 'W', 'X', 'Y', 'Z'}
+    };
+
+    int numberOfRows = polybiusSquare[0].length;
+    int numberOfColumns = polybiusSquare.length;
 
     @Override
     public String crypt(String inputWord) {
-        // TODO Auto-generated method stub
-        return null;
-    }
+        String str = inputWord.toUpperCase();
+        String output = "";
 
-    @Override
-    public String decrypt(String inputWord) {
-        // TODO Auto-generated method stub
-        return null;
+        for(int i=0; i< inputWord.length(); i++){
+            if(str.charAt(i)<'A' | str.charAt(i)>'Z'){
+                output += str.charAt(i);
+            }
+            for(int j=0; j<numberOfRows; j++){ 
+                for(int k=0; k<numberOfColumns; k++){
+                    if(str.charAt(i)==polybiusSquare[j][k]){
+                        output += (char)j+1;
+                        output +=  + (char)k+1;
+                    }
+                }
+            }
+        }
+        return output;
     }
     
+    @Override
+    public String decrypt(String inputWord) {
+        String output = "";
+        for(int i=0; i<inputWord.length(); i++){
+            if(inputWord.charAt(i)<'0' | inputWord.charAt(i)>'9') { 
+                output += inputWord.charAt(i);
+            }
+            else {
+                int row = inputWord.charAt(i)- '0' - 1;
+                int col = inputWord.charAt(i+1)- '0' - 1;
+                output += polybiusSquare[row][col]; 
+                i+=1;
+            }
+        }
+        return output;
+    }
 }
