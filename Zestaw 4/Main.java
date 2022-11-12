05a439f79f5155b77315a9fad599ffdc080ff21d
DominikaMaciag <Dominika.s45@wp.pl>
2022-11-12 16:41:24 +0100

    implemented ROT11 methods

--- Zestaw 4/Main.java
+++ Zestaw 4/Main.java
@@ -2,7 +2,13 @@ import java.io.FileNotFoundException;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        Cryptographer obj1 = new Cryptographer();
-        obj1.cryptfile("tocrypt.txt", "decrypt.txt", null);
+        // Cryptographer obj1 = new Cryptographer();
+        // obj1.cryptfile("tocrypt.txt", "decrypt.txt", null);
+
+        String test = "I 12 lat.";
+        String test2 = "T CD wl4.";
+        ROT11 r1 = new ROT11();
+        System.out.println(r1.crypt(test));
+        System.out.println(r1.decrypt(test2));
     }
 }
--- Zestaw 4/ROT11.java
+++ Zestaw 4/ROT11.java
@@ -1,17 +1,39 @@
 public class ROT11 implements Algorithm{
     String alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
     int rotation = 11;
+    String changed = "";
     
     @Override
     public String crypt(String inputWord) {
-        // TODO Auto-generated method stub
-        return null;
+        for(int i=0; i< inputWord.length(); i++){
+            if(inputWord.charAt(i)<'0' | inputWord.charAt(i)>'9' & inputWord.charAt(i)<'A' | inputWord.charAt(i)>'Z'
+            & inputWord.charAt(i)<'a' | inputWord.charAt(i)>'z'){
+                changed += inputWord.charAt(i);
+            }
+            for(int j=0; j< alphabet.length(); j++){
+                if(inputWord.charAt(i)==alphabet.charAt(j)){
+                    changed += alphabet.charAt((j+rotation)%alphabet.length());
+                }
+            }
+        }
+        return changed;
     }
 
     @Override
     public String decrypt(String inputWord) {
-        // TODO Auto-generated method stub
-        return null;
+        changed= "";
+        for(int i=0; i< inputWord.length(); i++){
+            if(inputWord.charAt(i)<'0' | inputWord.charAt(i)>'9' & inputWord.charAt(i)<'A' | inputWord.charAt(i)>'Z'
+            & inputWord.charAt(i)<'a' | inputWord.charAt(i)>'z'){
+                changed += inputWord.charAt(i);
+            }
+            for(int j=0; j< alphabet.length(); j++){
+                if(inputWord.charAt(i)==alphabet.charAt(j)){
+                    changed += alphabet.charAt((j-rotation+alphabet.length())%alphabet.length());
+                }
+            }
+        }
+        return changed;
     }
     
 }
