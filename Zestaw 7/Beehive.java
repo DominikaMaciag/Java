b0502ef1eb120f90dab8f18037ac3a1feb740798
DominikaMaciag <Dominika.s45@wp.pl>
2023-01-07 23:02:03 +0100

    update

--- Zestaw 7/Beehive.java
+++ Zestaw 7/Beehive.java
@@ -1,21 +1,48 @@
 public class Beehive {
     public int available = 2; // dostępne miejsca w ulu 
+    Boolean firstEntranceFree = true; // pszczolka moze wleciec jak jest true
+    Boolean secondEntranceFree = true;
 
     // gdy pszczoła wlatuje:
-    void flying_in(){
+    // pszczoly najpierw sprawdzają wejście nr 1 dopiero później wejście nr 2
+    int flying_in(int id){
         while(true){
             synchronized(this){
                 if(available>0){
+                    System.out.println("Pszczoła " + id + " podlatuje pod przelot 1");
                     available --;
-                    break;                    
+                    if (firstEntranceFree == true){
+                        System.out.println("Pszczoła " + id + " wlatuje przez przelot 1");
+                        firstEntranceFree = false;
+                        return 1;
+                    }
+                    if (firstEntranceFree == false) {
+                        System.out.println("Pszczoła " + id + " przelot 1 zajęty, sprawdza przelot 2");
+                    }
+                    if (secondEntranceFree == true){
+                        System.out.println("Pszczoła " + id + " wlatuje przez przelot 2");
+                        secondEntranceFree = false;
+                        return 2;
+                    }    
+                    if (secondEntranceFree == false){
+                        System.out.println("Pszczoła " + id + " przelot 2 zajęty, oczekuję przelot 2");
+                    }              
                 }
             }
         }
     }
 
     // gdy pszczoła wylatuje
-    void flying_out(){
+    void flying_out(int entrance, int id){
         synchronized(this){
+            if ( entrance == 1){
+                firstEntranceFree = true;
+                System.out.println("Pszczoła " + id + " wylatuje z przelotu 1");
+            }
+            else {
+                secondEntranceFree = true;
+                System.out.println("Pszczoła " + id + " wylatuje z przelotu 2");
+            }
             available ++;
         }
     }
--- Zestaw 7/Bees.java
+++ Zestaw 7/Bees.java
@@ -6,16 +6,19 @@ public class Bees extends Thread{
     int id;
     
     public void run(){
+        int entrance = 0;
+
         try {
             while(true){
-                beehive.flying_in();
-                passing_through();
-                beehive.flying_out();
-                staying_in();
-                beehive.flying_in();
-                passing_through();
-                beehive.flying_out();
-                staying_out();  
+                // przyjmuję, że pszczoły zaczynają od próby wlecenia do któregoś z wejść ula
+                entrance = beehive.flying_in(id);
+                passing_through(); // pszczoła przechodzi przez wejście
+                beehive.flying_out(entrance, id); // pszczoła przeszła przez wejście
+                staying_in(); // pszczoła czeka w ulu
+                entrance = beehive.flying_in(id); // pszczoła wybiera wyjście
+                passing_through(); // pszczoła przechodzi przez wyjście
+                beehive.flying_out(entrance, id); // pszczoła przeszła przez wyjście
+                staying_out();  // pszczoła zostaje na zewnątrz 
             }
             
         } catch (InterruptedException e) {
@@ -26,7 +29,7 @@ public class Bees extends Thread{
 
     // wejście lub wyjście z ula
     void passing_through() throws InterruptedException{
-        System.out.println("Pszczola o id: "+ id + " przechodzi przez drzwi");
+        // System.out.println("Pszczola o id: "+ id + " przechodzi przez drzwi");
         TimeUnit.SECONDS.sleep(1); // pszczoła wlatuje/wylatuje przez 1 sekundę 
     }
 
@@ -34,13 +37,13 @@ public class Bees extends Thread{
     void staying_in() throws InterruptedException{
         int randomNum = ThreadLocalRandom.current().nextInt(1, 5 + 1);
         TimeUnit.SECONDS.sleep(randomNum);
-        System.out.println("Pszczola o id: "+ id + " przebywa w ulu " + randomNum + " sekund" );
+        // System.out.println("Pszczola o id: "+ id + " przebywa w ulu " + randomNum + " sekund" );
     }
 
     // przebywa na zewnątrz 5-10 sekund:
     void staying_out() throws InterruptedException{
         int randomNum2 = ThreadLocalRandom.current().nextInt(5, 10 + 1);
         TimeUnit.SECONDS.sleep(randomNum2);
-        System.out.println("Pszczola o id: "+ id + " przebywa na zewnątrz " + randomNum2 + " sekund");
+        // System.out.println("Pszczola o id: "+ id + " przebywa na zewnątrz " + randomNum2 + " sekund");
     }
 }
